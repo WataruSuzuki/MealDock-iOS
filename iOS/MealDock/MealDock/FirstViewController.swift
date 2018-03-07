65ef732d34683802fd0d04fe8c0f03665831ec9a
WataruSuzuki <wataru0406@gmail.com>
2018-03-07 23:10:00 +0900

    Created xcode project

---
+++ iOS/MealDock/MealDock/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MealDock
+//
+//  Created by 鈴木 航 on 2018/07/30.
+//  Copyright © 2018年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS/MealDock/MealDock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MealDock
+//
+//  Created by 鈴木 航 on 2018/07/30.
+//  Copyright © 2018年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
