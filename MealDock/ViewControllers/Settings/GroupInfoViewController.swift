131fe2bfabbff22d011cd5ce9cca1f0edbe1ff26
WataruSuzuki <wataru0406@gmail.com>
2018-10-15 21:22:57 +0900

    Modified function that delete member from table view

--- MealDock/ViewControllers/Settings/GroupInfoViewController.swift
+++ MealDock/ViewControllers/Settings/GroupInfoViewController.swift
@@ -90,7 +90,8 @@ class GroupInfoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            FirebaseService.shared.deleteDockMember(memberId: "hogefuga")
+            FirebaseService.shared.deleteDockMember(memberId: dockMembers[indexPath.row].id)
+            dockMembers.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         //} else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
