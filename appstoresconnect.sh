062b75d47c6f1b473e9385927ba7a2f522e8db17
WataruSuzuki <wataru0406@gmail.com>
2018-10-27 01:12:35 +0900

    Defined provisioning profile for releasing appstore

--- appstoresconnect.sh
+++ appstoresconnect.sh
@@ -19,8 +19,7 @@ fi
 # 関数の定義
 # -------------------------
 appstore_connect () {
-    xcodebuild clean -workspace "${PROJ_FILE_PATH}"
-    xcodebuild archive -workspace "${PROJ_FILE_PATH}" -scheme $1 -archivePath "${PWD}/${OUT_ARCHIVES_DIR}/$1.xcarchive" -configuration $2
+    xcodebuild clean archive -workspace "${PROJ_FILE_PATH}" -scheme $1 -archivePath "${PWD}/${OUT_ARCHIVES_DIR}/$1.xcarchive" -configuration $2
     xcodebuild -exportArchive -archivePath "${PWD}/${OUT_ARCHIVES_DIR}/$1.xcarchive" -exportPath "${PWD}/${OUT_IPA_DIR}" -exportOptionsPlist "${PWD}/exportOptions.Plist"
     "${ALTTOOL}" --validate-app -f "${PWD}/${OUT_IPA_DIR}/$1.ipa"  -u ${ITUNES_ID} -p ${ITUNES_PW}
     "${ALTTOOL}" --upload-app -f "${PWD}/${OUT_IPA_DIR}/$1.ipa"  -u ${ITUNES_ID} -p ${ITUNES_PW}
