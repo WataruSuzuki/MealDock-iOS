852b7047a016a51e543e5e1116aa73beedfb6b47
WataruSuzuki <wataru0406@gmail.com>
2018-12-13 23:20:00 +0900

    Updated test scripts

--- test_all.sh
+++ test_all.sh
@@ -1,14 +1,14 @@
-xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone XS Max' \
-    -destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (2nd generation)' \
+./test_primary.sh
 
 xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPad Pro (10.5-inch),OS=11.4' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=11.4' \
+    > test_os11.log
 
 xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPad Air 2,OS=10.3.1' \
     -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1' \
+    > test_os10.log
 
 xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 5s,OS=9.3' \
