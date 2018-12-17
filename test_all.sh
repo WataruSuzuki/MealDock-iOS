e835f6d4d73df5adf260ecde7949dd99793b5092
WataruSuzuki <wataru0406@gmail.com>
2018-12-17 22:20:00 +0900

    Enabled japanese localization

--- test_all.sh
+++ test_all.sh
@@ -1,15 +1,15 @@
 ./test_primary.sh
 
 xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPad Pro (10.5-inch),OS=11.4' \
-    -destination 'platform=iOS Simulator,name=iPhone X,OS=11.4' \
+    -destination 'platform=iOS Simulator,name=iPad (5th generation) (11.4)' \
+    -destination 'platform=iOS Simulator,name=iPhone X (11.4)' \
     > test_os11.log
 
 xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPad Air 2,OS=10.3.1' \
-    -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1' \
+    -destination 'platform=iOS Simulator,name=iPad Pro (10.5-inch) (10.3.1)' \
+    -destination 'platform=iOS Simulator,name=iPhone 7 Plus (10.3.1)' \
     > test_os10.log
 
 xcodebuild test -workspace MealDock.xcworkspace -scheme MealDock -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone 5s,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3.1' \
+    -destination 'platform=iOS Simulator,name=iPhone 4s (9.3)' \
+    -destination 'platform=iOS Simulator,name=iPhone 6 (9.3)' \
