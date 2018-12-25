93d6999c9133aed8b164183bb98e102abad21389
WataruSuzuki <wataru0406@gmail.com>
2018-12-25 22:50:00 +0900

    Updated extension for UI Testing

--- MealDockUITests/Extensions/XCTestCase+Localization.swift
+++ MealDockUITests/Extensions/XCTestCase+Localization.swift
@@ -9,16 +9,8 @@
 import XCTest
 
 extension XCTestCase {
-    class func getTestStr(key: String, sender: AnyClass, bundleClass: AnyClass) -> String {
-        if let languageBundlePath = Bundle(for: sender).path(forResource: NSLocale.current.languageCode!, ofType: "lproj") {
-            if let localizationBundle = Bundle(path: languageBundlePath) {
-                return NSLocalizedString(key, bundle:localizationBundle, comment: "")
-            }
-        }
-        return NSLocalizedString(key, bundle:Bundle(for: bundleClass), comment: "")
-    }
     
-    class func isJapanese(sender: AnyClass, bundleClass: AnyClass) -> Bool {
-        return getTestStr(key: "cancel", sender: sender, bundleClass: bundleClass) == "キャンセル"
+    class func isJapanese(sender: AnyClass) -> Bool {
+        return "cancel".localized(for: sender) == "キャンセル"
     }
 }
